b09527871d9dd20589b4f723ae8f864da822333f
Fam-S <113306078+Fam-S@users.noreply.github.com>
2026-04-10 18:56:16 +0200

    Inference Preprocessing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 import yaml
+import cv2
+from src.preprocess import get_template, get_search_region, preprocess_image
 
 
 def load_config(config_path: str):
@@ -13,9 +15,56 @@ def main():
     args = parser.parse_args()
 
     config = load_config(args.config)
+
     print("Inference config loaded successfully:")
     for k, v in config.items():
         print(f"{k}: {v}")
+
+    video_path = config["video_path"]
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        print(" Error: Could not open video")
+        return
+
+    print("Video opened successfully")
+
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to read first frame")
+        return
+
+    bbox = cv2.selectROI("Select Target", frame, False, False)
+    print("Selected BBox:", bbox)
+
+    cv2.destroyWindow("Select Target")
+
+    template = get_template(frame, bbox)
+    template = preprocess_image(template)
+
+    print("Template shape:", template.shape)
+
+    cv2.imshow("Template", template)
+    cv2.waitKey(1000) 
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        search = get_search_region(frame, bbox)
+        search = preprocess_image(search)
+
+        x, y, w, h = map(int, bbox)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imshow("Tracking", frame)
+
+        if cv2.waitKey(30) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
